48fdd6a12a420ed0d539665f11ca399e4f3b1c3e
snuppy <snuppy.code+git@pm.me>
2026-07-26 14:06:55 +0200

    ehhh prepare to restructure

--- src/lexer_errors.rs
+++ src/lexer_errors.rs
@@ -16,62 +16,59 @@ impl<'i> Display for LexerError<'i> {
 
 #[derive(Debug, PartialEq)]
 pub enum LexerErrorKind {
-    UnclosedLongComment,
-
-    InvalidStringuEscapeMissingOpenBrace,
-    InvalidStringuEscapeMissingClosingBrace,
-    InvalidStringuEscapeInvalidChar(u8),
-    InvalidStringuEscapeNoDigits,
-    InvalidStringuEscapeBeyond0x10FFFF,
-
-    InvalidStringxEscapeUnfinished,
-    InvalidStringxEscapeInvalidPossiblyTooLarge,
-
-    InvalidStringDecimalEscapeBeyondu8,
-    InvalidStringEscape,
-    UnclosedShortString,
-    UnclosedLongString,
-    DecimalNumericConstantNeedsIntOrFracPart,
-    HexNumericConstantNeedsIntOrFracPart,
-    HexNumericConstantMalformedExponent,
-    DecimalNumericConstantMalformedExponent,
+    UnclosedLongComment,                         // from to rest of file
+    InvalidStringuEscapeMissingOpenBrace,        // from until after 4 spaces
+    InvalidStringuEscapeMissingClosingBrace,     // from start of until after 2 spaces
+    InvalidStringuEscapeInvalidChar(u8),         // this token
+    InvalidStringuEscapeNoDigits,                // this token
+    InvalidStringuEscapeBeyond0x10FFFF,          // this token
+    InvalidStringxEscapeUnfinished,              // from start until after 2 spaces
+    InvalidStringxEscapeInvalidPossiblyTooLarge, // this token
+    InvalidStringDecimalEscapeBeyondu8,          // this token
+    InvalidStringEscape,                         // from start until after 1 space
+    UnclosedShortString,                         // this line
+    UnclosedLongString,                          // from to rest of file
+    DecimalNumericConstantNeedsIntOrFracPart,    // from start until after 3 spaces
+    HexNumericConstantNeedsIntOrFracPart,        // from start until after 3 spaces
+    HexNumericConstantMalformedExponent,         // from start until after 2 spaces
+    DecimalNumericConstantMalformedExponent,     // from start until after 2 spaces
 }
 
 impl Display for LexerErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             // Self::FailedLexingAll(s) => write!(f,"Encountered unexpected input! remains: `{}`",s),
-            Self::UnclosedLongComment => write!(f, "Unclosed long comment !"),
+            Self::UnclosedLongComment => write!(f, "Unclosed long comment"),
 
             Self::InvalidStringuEscapeMissingOpenBrace => {
-                write!(f, "Invalid \\u string escape: Missing opening brace !")
+                write!(f, "Invalid \\u string escape: Missing opening brace")
             }
             Self::InvalidStringuEscapeMissingClosingBrace => {
-                write!(f, "Invalid \\u string escape: Missing closing brace !")
+                write!(f, "Invalid \\u string escape: Missing closing brace")
             }
             Self::InvalidStringuEscapeInvalidChar(b) => write!(
                 f,
-                "Invalid \\u string escape: Invalid character (byte value: {b}) in escape sequence !"
+                "Invalid \\u string escape: Invalid character (byte value: {b}) in escape sequence"
             ),
             Self::InvalidStringuEscapeNoDigits => write!(
                 f,
-                "Invalid \\u string escape: No digits in escape sequence ! (needed to specify codepoint)"
+                "Invalid \\u string escape: No digits in escape sequence (needed to specify codepoint)"
             ),
             Self::InvalidStringuEscapeBeyond0x10FFFF => write!(
                 f,
-                "Invalid \\u string escape: Beyond Lua 5.3 0x10FFFF limit !"
+                "Invalid \\u string escape: Beyond Lua 5.3 0x10FFFF limit"
             ),
 
             Self::InvalidStringxEscapeUnfinished => {
-                write!(f, "Unfinished \\x escape sequence at end of string !")
+                write!(f, "Unfinished \\x escape sequence at end of string")
             }
             Self::InvalidStringxEscapeInvalidPossiblyTooLarge => {
-                write!(f, "Invalid hex, maybe too large? in \\x escape sequence !")
+                write!(f, "Invalid hex, maybe too large? in \\x escape sequence")
             }
 
             Self::DecimalNumericConstantNeedsIntOrFracPart => write!(
                 f,
-                "Decimal numeric constant needs at least an integer or fractional part !"
+                "Decimal numeric constant needs at least an integer or fractional part"
             ),
             Self::HexNumericConstantNeedsIntOrFracPart => write!(
                 f,
@@ -85,11 +82,11 @@ impl Display for LexerErrorKind {
             }
 
             Self::InvalidStringDecimalEscapeBeyondu8 => {
-                write!(f, "Invalid decimal string escape: Beyond 255 !")
+                write!(f, "Invalid decimal string escape: Beyond 255")
             }
-            Self::InvalidStringEscape => write!(f, "Unrecognized string escape !"),
-            Self::UnclosedShortString => write!(f, "Unclosed short string !"),
-            Self::UnclosedLongString => write!(f, "Unclosed long string !"),
+            Self::InvalidStringEscape => write!(f, "Unrecognized string escape"),
+            Self::UnclosedShortString => write!(f, "Unclosed short string"),
+            Self::UnclosedLongString => write!(f, "Unclosed long string"),
         }
     }
 }
